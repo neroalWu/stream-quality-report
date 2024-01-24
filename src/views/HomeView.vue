b088d604b6a587a2a11dc71c87cebd43f4d6d8e1
Neroal Wu <neroal.Wu@north.com.tw>
2024-01-24 18:18:16 +0800

    change color theme

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,10 @@ import StreamQualityReport from "@/components/StreamQualtiyReport.vue"
 </script>
 
 <template>
+    <div class="header">
+        <img class="logo" src="../assets/logo.png" alt="">
+    </div>
+
     <main>
         <StreamQualityReport/>
     </main>
