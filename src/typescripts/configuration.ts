01c920c544267dac9fd206d4f6de1c6e359d554f
Neroal Wu <neroal.Wu@north.com.tw>
2024-01-26 14:46:57 +0800

    add type declare and remove config

--- src/typescripts/configuration.ts
+++ src/typescripts/configuration.ts
@@ -1,19 +1,6 @@
 const STREAM_QUALITY_REPORT = {
     API_URI: 'https://www.google.com',
     QUERY_INTERVAL: 600000, // 10mins
-
-    CEBU: {
-        RTMP: {
-            SERVER_URL: 'http://10.20.30.130:8000/rtmp/calculate_topiq',
-            STREAM_URL: 'rtmp://10.22.1.201/cb/',
-            CHANNELS: ['BTCB02', 'BTCB02m', 'BTCB03', 'BTCB03m']
-        },
-        FLV: {
-            SERVER_URL: 'http://10.20.30.130:8000/flv/calculate_topiq',
-            STREAM_URL: '',
-            CHANNELS: []
-        }
-    }
 }
 
 export const CONFIGURATION = {
