1e2f8530e9b459ffabcd16739b5f1ad045491bd4
Neroal Wu <neroal.Wu@north.com.tw>
2024-02-05 15:01:39 +0800

    rename folder

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import RecordView from '../views/RecordView.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
-            name: 'home',
-            component: HomeView
+            name: 'record',
+            component: RecordView
         },
         {
           path: '/stream',
