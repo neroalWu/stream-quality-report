55df7a5f545a96f8aa83d6858f756fadb760da02
Neroal Wu <neroal.Wu@north.com.tw>
2024-02-06 13:24:26 +0800

    rename module

--- src/typescripts/response/detail-response.ts
+++ src/typescripts/response/detail-response.ts
@@ -1,5 +1,5 @@
 import type DetailData from '../data/detail-data'
 
 export default class DetailResponse {
-    constructor(public details: DetailData[]) {}
+    constructor(public detail: DetailData) {}
 }
--- src/views/StreamView.vue
+++ src/views/StreamView.vue
@@ -1,22 +1,26 @@
 <script setup lang="ts">
 import DetailRequest from '@/typescripts/request/detail-request'
+import type DetailResponse from '@/typescripts/response/detail-response';
 import HttpService from '@/typescripts/service/http-service'
 import Store from '@/typescripts/store/store'
+import { ref } from 'vue'
+
+const detail = ref<DetailResponse>()
 
 async function main() {
     const detailRequest = DetailRequest.Create(
         Store.Instance.selectedSummary,
         Store.Instance.selectedRangeDate
     )
-    const detailsResponse = await HttpService.Instance.GetDetails(detailRequest)
-    console.log('neroal:', detailsResponse)
+    const detailResponse = await HttpService.Instance.GetDetails(detailRequest)
+    detail.value = detailResponse
 }
 
 main()
 </script>
 
 <template>
-    <body></body>
+    <body>{{ detail }}</body>
 </template>
 
 <style scoped>
